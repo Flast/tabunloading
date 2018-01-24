7ab9cf41a28fd620f1c7614e4e41c4fea398fdf3
Kohei Takahashi <flast@flast.jp>
2018-01-24 21:42:21 +0900

    A first implementation.

---
+++ tabunloading.js
@@ -0,0 +1,33 @@
+// Copyright 2018 Kohei Takahshi
+//
+// This software is released under the MIT License, see LICENSE.txt.
+
+function onCreated() {
+  if (browser.runtime.lastError) {
+    console.log(`Error: ${browser.runtie.lastError}`);
+  }
+}
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+browser.menus.create({
+  id: "unload-me",
+  title: browser.i18n.getMessage("unloadSingleTab"),
+  // TODO: Recognize tree-style tab addon.
+  contexts: ["tab"]
+}, onCreated);
+
+function unloadSingleTab(tab) {
+  // FIXME: Unable to discard current (active) tab.
+  browser.tabs.discard(tab.id).then(null, onError);
+}
+
+browser.menus.onClicked.addListener((info, tab) => {
+  switch (info.menuItemId) {
+    case "unload-me":
+      unloadSingleTab(tab);
+      break;
+  }
+});
