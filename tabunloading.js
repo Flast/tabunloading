99a90b90b1c1e4d9ea9d5d538d17f1304db196a3
Kohei Takahashi <flast@flast.jp>
2019-04-11 21:52:22 +0900

    Initial support for treestyletab

--- tabunloading.js
+++ tabunloading.js
@@ -2,6 +2,52 @@
 //
 // This software is released under the MIT License, see LICENSE.txt.
 
+// Initialize tree style tab
+const TST_ID = 'treestyletab@piro.sakura.ne.jp';
+
+async function registerToTST() {
+  await browser.runtime.sendMessage(TST_ID, {
+    type: 'register-self',
+    name: browser.i18n.getMessage('extensionName'),
+    icons: browser.runtime.getManifest().icons,
+    listeningTypes: ['ready'],
+  });
+
+  return function(request) {
+    return browser.runtime.sendMessage(TST_ID, request);
+  };
+}
+let TST_sendMessage = registerToTST();
+
+browser.runtime.onMessageExternal.addListener((message, sender) => {
+  switch (sender.id) {
+    case TST_ID:
+      switch (message.type) {
+        case 'ready':
+          TST_sendMessage = registerToTST();
+          onTSTReady();
+          break;
+      }
+      break;
+  }
+});
+
+function onTSTReady() {
+  TST_sendMessage.then((send) => {
+    return send({type: 'wait-for-shutdown'});
+  }).finally(() => {
+    TST_sendMessage = new Promise(() => { throw 'Unloaded'; });
+    onTSTShutdown();
+  });
+
+  browser.menus.update(menu_unloadTreeTST, {visible: true});
+}
+
+function onTSTShutdown() {
+  browser.menus.update(menu_unloadTreeTST, {visible: false});
+}
+
+
 function onCreated() {
   if (browser.runtime.lastError) {
     console.log(`Error: ${browser.runtime.lastError}`);
@@ -49,3 +95,5 @@ browser.menus.create({
   contexts: ["tab"],
   onclick: unloadAllTabsInWindow
 }, onCreated);
+
+TST_sendMessage.then(onTSTReady); // This line should be last
